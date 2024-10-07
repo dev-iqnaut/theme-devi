c4f308160234440b9bf48dfa5d73e905bdff0084
kumaradevi <rskumaradevi95@gmail.com>
2024-10-07 10:29:17 +0530

    including firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAMFY2-q-RHTjNaQaxROgS34YSUZEyb8g",
+  authDomain: "kumaradevi-s-schooldesign.firebaseapp.com",
+  projectId: "kumaradevi-s-schooldesign",
+  storageBucket: "kumaradevi-s-schooldesign.appspot.com",
+  messagingSenderId: "701345413961",
+  appId: "1:701345413961:web:851b192c72f918370ab898"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
